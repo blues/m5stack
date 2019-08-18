c859aa641df392950138c30e70178d42053dc16d
John Wiedey <jwiedey@blues.com>
2019-08-18 00:13:38 -0400

    App now reports temp/voltage/count like note-arduino instead of a random number.

--- m5note/src/actions/test.cpp
+++ m5note/src/actions/test.cpp
@@ -26,11 +26,49 @@ int actionTestToggleContinuous(int buttonState) {
     return MENU_ACTION_DISMISS;
 }
 
-// Add a random test note to a test file, simulating a temperature from -5.00 to 85.00 degrees
+// Add a Note to the file "test.qo"
 int actionTestAdd(int buttonState) {
+    static unsigned messagesSent = 0;
     J *body = JCreateObject();
     if (body != NULL) {
-        JAddNumberToObject(body, "temperature", (double) random(-500, 8500) / 100);
+        // Use the temperature sensor on the Notecard to get a sample temperature measurement
+        double temperature;
+        // This results in a JSON message to Notecard formatted like:
+        //     { "req"     : "card.temp" }
+        NoteGetTemperature(&temperature);
+        // Insert the temperature measurement into the body of our Note
+        JAddNumberToObject(body, "temperature", temperature);
+
+        // Use the voltage ADC on the Notecard to get a sample voltage measurement
+        double voltage;
+        // This results in a JSON message to Notecard formatted like:
+        //     { "req"     : "card.voltage" }
+        NoteGetVoltage(&voltage);
+        // Insert the voltage measurement into the body of the Note
+        JAddNumberToObject(body, "voltage", voltage);
+
+        // Insert incrementing message count into the body of the Note
+        JAddNumberToObject(body, "count", ++messagesSent);
+
+        // EXAMPLE OF HOW TO INSERT A BOOL INTO THE BODY OF THE NOTE
+        // Jbool truth = true;
+        // JAddBoolToObject(body, "truth", truth);
+
+        // EXAMPLE OF HOW TO INSERT A STRING INTO THE BODY OF THE NOTE
+        // JAddStringToObject(body, "string", "value");
+
+        // EXAMPLE OF HOW TO INSERT AN ARRAY INTO THE BODY OF THE NOTE
+        // float myFloatArray[] = { 2.71828, 3.14159, 42 };
+        // J *array = JCreateFloatArray(myFloatArray, sizeof(myFloatArray)/sizeof(myFloatArray[0]));
+        // JAddItemToObject(body, "array", array);
+
+        // EXAMPLE OF HOW TO INSERT AN OBJECT INTO THE BODY OF THE NOTE
+        // J *object = JAddObjectToObject(body, "object");
+        // JAddStringToObject(object, "first",  "Who");
+        // JAddStringToObject(object, "second", "What");
+        // JAddStringToObject(object, "third",  "I Don't Know");
+
+        // Send the Note to the Notecard file "test.qo", but do not Sync immediately
         NoteSend("test.qo", body, false);
     }
     return MENU_ACTION_DISMISS;
